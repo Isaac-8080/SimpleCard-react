93d2b3002ce934416c1c37848c2a26d383f7d279
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-11-04 18:07:48 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SimpleCard from './components/SimpleCard'
+
+function App() {
+
+  return (
+    <>
+    <div style={{}} className='app__wrapper'>
+      <SimpleCard />
+    </div>
+    </>
+  )
+}
+
+export default App
